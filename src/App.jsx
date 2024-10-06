115edd9913bfa92b9f5484dfef0ff8091e59932e
Sachith Priyashantha <sachithpriyashantha24@gmail.com>
2024-10-06 15:50:28 +0530

    Fetch Data from API and using useState, useEffect Hooks to manage Loaded Data

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,47 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [todoList, setTodoList] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  async function fetchDataFromAPI() {
+    try {
+      setLoading(true)
+      const apiData = await fetch('https://dummyjson.com/todos')
+      const result = await apiData.json()
+
+      console.log(result);
+
+      if (result?.todos && result?.todos.length > 0) 
+      {
+        setTodoList(result?.todos)
+        setLoading(false)
+      }
+      else 
+      {
+        setTodoList([])
+        setLoading(false)
+      }
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchDataFromAPI()
+  }, [])
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>
+          Simple Todo App
+        </h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
